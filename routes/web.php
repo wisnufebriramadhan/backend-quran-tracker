344e0823d2b2ee1fb559de0e4d6f3ff401f4e240
wisnufebri <wisnufebriramadhan@gmail.com>
2025-12-26 10:15:29 +0700

    add landing page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Http\Controllers\Admin\QuranLogController;
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::view('/', 'welcome');
+
 
 /*
 |--------------------------------------------------------------------------
